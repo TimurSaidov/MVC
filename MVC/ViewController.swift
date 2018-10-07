fde98219cb889bac2e6c84b841bc7634b3963d26
TimurSaidov <tim77777@inbox.ru>
2018-10-07 15:33:30 +0300

    Initial Commit

---
+++ MVC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVC
+//
+//  Created by Timur Saidov on 07/10/2018.
+//  Copyright © 2018 Timur Saidov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
